47cf19cb42c15a0cc67e2dc4de081e4003713113
VanshUKnow <vanshrathodpay@gmail.com>
2025-07-20 19:41:24 +0530

    Update train_model.py

--- xai-heart-diagnosis/src/train_model.py
+++ xai-heart-diagnosis/src/train_model.py
@@ -4,23 +4,23 @@ import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# Load data
+#For loading the data
 df = pd.read_csv('data/heart_cleveland_upload.csv')
 
-# Replace 'target' with 'condition'
+#Replacing 'target' with 'condition' (because the end result in dataset that we used is target. Changing it to condition will be better decision)
 X = df.drop('condition', axis=1)
 y = df['condition']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train model
+#Train model
 model = xgb.XGBClassifier(use_label_encoder=False, eval_metric='logloss')
 model.fit(X_train, y_train)
 
-# Evaluate
+#Evaluate
 preds = model.predict(X_test)
 print(f"Accuracy: {accuracy_score(y_test, preds):.4f}")
 
-# Save model
+#Save model
 with open('models/xgb_model.pkl', 'wb') as f:
     pickle.dump(model, f)
